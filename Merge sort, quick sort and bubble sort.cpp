0bd44d2ac6dc8b82b12fa5d7cd0d9d27b9279972
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-01-22 20:04:21 +0800

    Add files via upload

---
+++ Merge sort, quick sort and bubble sort.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+int ri[100000],a[100000];
+long long x=0;
+void ef(int l,int r);
+void kp(int l,int r);
+void mp(int n);
+int main()
+{
+	int n;
+	cin>>n;
+	for (int i=1;i<=n;i++) 
+	{
+		cin>>a[i];
+	}
+	//ef(1,n);
+	//kp(1,n);
+	//mp(n);
+	for (int i=1;i<=n;i++) 
+	{
+		cout<<a[i]<<' ';
+	}
+	return 0;
+}
+void ef(int l,int r)
+{
+	if (l==r) return;
+	int i=l,mid=(l+r)/2,j=(l+r)/2+1,k=l;
+	ef(l,mid);
+	ef(j,r);
+	while (i<=mid&&j<=r)
+	{
+		if (a[i]<=a[j]) 
+		{
+			ri[k]=a[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			ri[k]=a[j];
+			k++;
+			j++;
+			x++;
+		}
+	}
+	while (i<=mid)
+	{
+		ri[k]=a[i];
+		k++;
+		i++;
+	} 
+	while (j<=r)
+	{
+		ri[k]=a[j];
+		k++;
+		j++;
+	}
+	for (int ii=l;ii<=r;ii++)
+		a[ii]=ri[ii];
+}
+void kp(int l,int r)
+{
+	int i=l,j=r,mid=(l+r)/2;
+	do
+	{
+		while (a[i]<a[mid]) i++;
+		while (a[j]>a[mid]) j--;
+		if (i<=j)
+		{
+			swap(a[i],a[j]);
+			j--;
+			i++;
+		}
+	}while(i<=j);
+	if (i<r) kp(i,r); 
+	if (l<j) kp(l,j);
+}
+void mp(int n)
+{
+	for (int i=1;i<n;i++)
+		for (int j=1;j<n;j++)
+			if (a[j]>a[j+1]) swap(a[j],a[j+1]);
+}
+
+
+
+
+
+
+
+
+
+
