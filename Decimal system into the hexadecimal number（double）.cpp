5095bb1f874611f613aa491b44ab5ece43a18fe0
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-01-26 09:42:14 +0800

    Update Decimal system into the hexadecimal number（double）.cpp

--- Decimal system into the hexadecimal number（double）.cpp
+++ Decimal system into the hexadecimal number（double）.cpp
@@ -1,1 +1,30 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+void zh(int x,int y);
+char s[36]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+int main()
+{
+	double a;
+	int k,i=1;
+	cin>>a>>k;
+	zh(k,(int)a);
+	cout<<".";
+	while (a>0&&i<=10)
+	{
+		a=(a-((int)a))*k;
+		int x=(int)a;
+		a-=x;
+		cout<<s[x];
+		i++;
+	}
+	return 0;
+}
+void zh(int x,int y)
+{
+	if (x>y) {cout<<s[y];return;}
+	int a=y%x;
+	y/=x;
+	zh(x,y);
+	cout<<s[a];
+}
