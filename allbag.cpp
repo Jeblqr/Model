f1bd8815bc972d32ca090272c160a0dc301c9ffa
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-03-20 16:20:40 +0800

    Update allbag.cpp

--- allbag.cpp
+++ allbag.cpp
@@ -2,17 +2,12 @@
 using namespace std;
 int main()
 {
-	int n,m,f[1000],c[1000],w[1000],g[1000];
+	int n,m,w[1000],c[1000],f[1000];
 	cin>>m>>n;
-	for (int i=1;i<=n;i++) 
-	{
-		cin>>w[i]>>c[i]>>g[i];
-		if (g[i]==0) g[i]=10000;
-	}
+	for (int i=1;i<=n;i++) cin>>w[i]>>c[i];
 	for (int i=1;i<=n;i++)
-		for (int j=m;j>=w[i];j--)
-			for (int k=1;k<=g[i];k++)
-				if (j-w[i]*k>=0&&f[j]<f[j-w[i]*k]+c[i]*k) f[j]=f[j-w[i]*k]+c[i]*k;
-	cout<<f[m];
+		for (int j=w[i];j<=m;j++)
+			if (f[j-w[i]]+c[i]>f[j]) f[j]=f[j-w[i]]+c[i];
+	cout<<"max="<<f[m];
 	return 0;
 }
