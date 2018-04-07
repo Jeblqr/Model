258ab40d84702b1f7a6abe05db7a3ed5041798d6
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-04-07 15:36:38 +0800

    Create 并查集.cpp

---
+++ 并查集.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int ancestors[11000000],n,m,ans;
+void init();
+void find_answer();
+int find_ancestors(int kid);
+void print();
+int main()
+{
+	init();
+	find_answer();
+	print();
+}
+void init()
+{
+	cin>>n>>m;
+	for (int i=1;i<=n;i++) 
+		ancestors[i]=i;
+}
+void find_answer()
+{
+	for (int i=1;i<=m;i++)
+	{
+		int x,y,fx,fy;
+		cin>>x>>y;
+		fx=find_ancestors(x);
+		fy=find_ancestors(y);
+		if (fx!=fy)
+			ancestors[fx]=fy;
+	}
+	for (int i=1;i<=n;i++) 
+		if (ancestors[i]==i)
+			ans++;
+}
+int find_ancestors(int kid)
+{
+	if (ancestors[kid]!=kid) ancestors[kid]=find_ancestors(ancestors[kid]);
+	return ancestors[kid];
+}
+void print()
+{
+	cout<<ans;
+}
