0c854492ab046cd0002d998c4e81c95e8c6dca47
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-08-20 21:13:02 +0800

    Update 图论算法-SPFA算法(Shortest Path Faster Algorithm)-最短路径.cpp

--- 图论算法-SPFA算法(Shortest Path Faster Algorithm)-最短路径.cpp
+++ 图论算法-SPFA算法(Shortest Path Faster Algorithm)-最短路径.cpp
@@ -1,70 +1,107 @@
 #include<iostream>
-#include<cstdio>
 #include<cstring>
+#include<cstdio>
 #include<string>
+#include<cstdlib>
 #include<algorithm>
-#include<queue>
 #include<set>
+#include<queue>
+#define Max1 1000000
+#define Max2 1000000
+#define Max3 Max1
 using namespace std;
-int last[1000000],len,x,y,d,n,m;
-deque<int> que;
-set<int> bj;
-double ans[1000000];
-class node
+
+
+class SPFA
 {
-	public:
-		int x,y,d,next;
-}a[1000000];
-inline void setting(int x,int y,int d)
+    class Pc
+    {
+	    bool ps[Max3];
+	    public:
+	    	bool count(int k);
+	    	void insert(int k);
+	    	void clear();
+		    bool erase(int k);
+    };
+    struct Node
+    {
+        int x,y,d,next;
+    };
+	Node node[Max1];
+    int last[Max2],len,ans[Max1];
+    Pc pd;
+    queue<int> que;
+    void MakeLine(int x,int y,int d);
+    public:
+        void scan(int n);
+        int calc(int x,int y);
+};
+
+bool SPFA::Pc::count(int k){return (ps[k]);}
+
+void SPFA::Pc::insert(int k){ps[k]=1;}
+
+void SPFA::Pc::clear(){memset(ps,0,sizeof ps);}
+
+bool SPFA::Pc::erase(int k){ps[k]=0;}
+
+void SPFA::MakeLine(int x,int y,int d)
 {
-	len++;
-	a[len].x=x,a[len].y=y,a[len].d=d;
-	a[len].next=last[x];
-	last[x]=len;
+    len++;
+    node[len].x=x,node[len].y=y,node[len].d=d,node[len].next=last[x];
+    last[x]=len;
 }
-void init()
+
+int SPFA::calc(int x,int y)
 {
-	cin>>n>>m;
-	for (int i=1;i<=m;i++)
-	{
-		cin>>x>>y>>d;
-		setting(x,y,d);
-		setting(y,x,d);
-	}	
+    memset(ans,0x7f,sizeof ans);
+    ans[x]=0;
+    while (que.size())
+        que.pop();
+    pd.clear();
+    que.push(x);
+    pd.insert(x);
+    while (!que.empty())
+    {
+        int kx=que.front();
+        que.pop();
+		pd.erase(kx);
+        for (int i=last[kx];i;i=node[i].next)
+        {
+            int ky=node[i].y;
+            if (ans[ky]>ans[kx]+node[i].d)
+            {
+                ans[ky]=ans[kx]+node[i].d;
+                if (!pd.count(ky))
+                {
+                    pd.insert(ky);
+                    que.push(ky);
+                }
+            }
+        }
+    }
+    return ans[y]==0x7f7f7f7f?-1:ans[y];
 }
-int SPFA(int x,int y)
+
+void SPFA::scan(int n)
 {
-	memset(ans,0x7f,sizeof ans);
-	ans[x]=0;
-	bj.insert(x);
-	que.push_back(x);
-	while (!que.empty())
-	{
-		int k=que.front();
-		que.pop_front();
-		for (int i=last[k];i;i=a[i].next)
-		{
-			if (ans[a[i].y]>ans[k]+a[i].d)
-			{
-				ans[a[i].y]=ans[k]+a[i].d;
-				if (!bj.count(a[i].y))
-				{
-					bj.insert(a[i].y);
-					que.push_back(a[i].y);
-				}
-			}
-		}
-		bj.erase(k);
-	}
-	return ans[y];
+    int x,y,v;
+    for (int i=1;i<=n;i++)
+    {
+        cin>>x>>y>>v;
+        MakeLine(x,y,v);
+        MakeLine(y,x,v);
+    }
 }
-void SPFA()
-{
-	init();
-	cout<<SPFA(1,n);
-}
+
+
+SPFA spfa;
+
 int main()
 {
-	SPFA();
-	return 0;
+    int n,m;
+    cin>>n>>m;
+    spfa.scan(m);
+    cout<<spfa.calc(1,n);
+    return 0;
 }
