d2728ec466d680dc4426eb7ef21f9d4e6f3514be
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-08-30 16:06:00 +0800

    Update 图论算法-并查集.cpp

--- 图论算法-并查集.cpp
+++ 图论算法-并查集.cpp
@@ -1,44 +1,84 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<queue>
+#include<stack>
 using namespace std;
-int ancestors[11000000],n,m,ans;
-void init();
-void find_answer();
-int find_ancestors(int kid);
-void print();
-int main()
+
+//---------------------------------------
+class Kruskal
+{
+	struct Node
+	{
+		int x,y,v;
+		bool operator<(const Node &x) const
+		{
+			return this->v<x.v;
+		}
+	}node[1100000];
+	int n,m,fa[1100000];
+	void init();
+	int find(int k);
+	public:
+		void set(int n);
+		void build(int x,int y,int v);
+		int get();
+};
+
+void Kruskal::set(int n)
+{
+	this->n=n;
+}
+
+void Kruskal::init()
+{
+	for (int i=1;i<=n;i++)
+		fa[i]=i;
+	sort(node+1,node+m+1);
+}
+
+int Kruskal::find(int k)
+{
+	if (fa[k]!=k)
+		fa[k]=find(fa[k]);
+	return fa[k];
+}
+
+void Kruskal::build(int x,int y,int v)
+{
+	m++;
+	node[m]=Node{x,y,v};
+}
+
+int Kruskal::get()
 {
 	init();
-	find_answer();
-	print();
-	return 0;
-}
-void init()
-{
-	cin>>n>>m;
-	for (int i=1;i<=n;i++) 
-		ancestors[i]=i;
-}
-void find_answer()
-{
+	int ans=0,t=0;
 	for (int i=1;i<=m;i++)
 	{
-		int x,y,fx,fy;
-		cin>>x>>y;
-		fx=find_ancestors(x);
-		fy=find_ancestors(y);
+		int fx=find(node[i].x),fy=find(node[i].y);
 		if (fx!=fy)
-			ancestors[fx]=fy;
+		{
+			fa[fx]=fy,ans+=node[i].v;
+			t++;
+			if (t==n-1)
+				break;
+		}
 	}
-	for (int i=1;i<=n;i++) 
-		if (ancestors[i]==i)
-			ans++;
+	return ans;
 }
-int find_ancestors(int kid)
+//---------------------------------------
+
+Kruskal kl;
+
+int main()
 {
-	if (ancestors[kid]!=kid) ancestors[kid]=find_ancestors(ancestors[kid]);
-	return ancestors[kid];
+	return 0;
 }
-void print()
-{
-	cout<<ans;
-}
+
+
