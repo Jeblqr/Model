7a8e8aa547333990b99e994bbc220a131b26a8f4
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-10-06 16:37:17 +0800

    Update 高级数据结构-线段树-区间和.cpp

--- 高级数据结构-线段树-区间和.cpp
+++ 高级数据结构-线段树-区间和.cpp
@@ -1,137 +1,103 @@
-#include <iostream>
-#include <cstring>
-#include <cstdio>
-#include <string>
-#include <cstdlib>
-#include <algorithm>
-#include <set>
-#include <cmath>
-#include <iomanip>
-#include <vector>
-#include <map>
-#include <queue>
-#define Max 500000
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#define Max 800000
 using namespace std;
-long long class ST
-{
-	struct Node
-	{
-		long long val, mark;
-	} tree[Max];
-	long long ls(long long k);
-	long long rs(long long k);
-	void PushUp(long long f, long long l, long long r, long long k);
-	void PushUpSum(long long f);
-	void PushUpMin(long long f);
-	void PushUpMax(long long f);
-	void PushDown(long long f, long long l, long long r);
 
-  public:
-	void init();
-	void build(long long f, long long a[], long long l, long long r);
-	long long query(long long f, long long i, long long j, long long l, long long r);
-	void updata(long long f, long long i, long long j, long long l, long long r, long long val);
+//----------------------------------------
+class St
+{
+	struct node
+	{
+		long long val,tag;
+	};
+	node Node[Max];
+	inline long long L(long long x);
+	inline long long R(long long x);
+	inline void PushUp(long long root);
+	inline void PushDown(long long root,long long l,long long r,long long k);
+	inline void PushSum(long long root,long long l,long long r);
+	public:
+		void build(long long root,long long l,long long r,long long *a);
+		void updata(long long root,long long l,long long r,long long i,long long j,long long k);
+		long long query(long long root,long long l,long long r,long long i,long long j);
 };
 
-long long ST::ls(long long k) { return k << 1; }
 
-long long ST::rs(long long k) { return k << 1 | 1; }
 
-void ST::PushUpSum(long long f) { tree[f].val = tree[ls(f)].val + tree[rs(f)].val; }
 
-void ST::PushUpMin(long long f) { tree[f].val = min(tree[ls(f)].val, tree[rs(f)].val); }
+inline long long St::L(long long x){return x<<1;}
 
-void ST::PushUpMax(long long f) { tree[f].val = max(tree[ls(f)].val, tree[rs(f)].val); }
+inline long long St::R(long long x){return L(x)|1;}
 
-void ST::PushUp(long long f, long long l, long long r, long long k)
+void St::PushUp(long long root)
 {
-	tree[f].val += (r - l + 1) * k;
-	tree[f].mark += k;
+	Node[root].val=Node[L(root)].val+Node[R(root)].val;
 }
 
-void ST::PushDown(long long f, long long l, long long r)
+void St::PushDown(long long root,long long l,long long r,long long k)
 {
-	long long mid = (l + r) / 2;
-	PushUp(ls(f), l, mid, tree[f].mark);
-	PushUp(rs(f), mid + 1, r, tree[f].mark);
-	tree[f].mark = 0;
+	Node[root].val+=k*(r-l+1);
+	Node[root].tag+=k;
 }
 
-void ST::init()
+void St::PushSum(long long root,long long l,long long r)
 {
-	memset(tree, 0, sizeof tree);
+	long long mid=(l+r)/2;
+	PushDown(L(root),l,mid,Node[root].tag);
+	PushDown(R(root),mid+1,r,Node[root].tag);
+	Node[root].tag=0;
 }
 
-void ST::build(long long f, long long a[], long long l, long long r)
+void St::build(long long root,long long l,long long r,long long *a)
 {
-	tree[f].mark = 0;
-	if (l == r)
+	if (l==r)
 	{
-		tree[f].val = a[l];
+		Node[root].val=a[l];
+		return;
+	}	
+	long long mid=(l+r)/2;
+	build(L(root),l,mid,a);
+	build(R(root),mid+1,r,a);
+	PushUp(root);
+}
+
+void St::updata(long long root,long long l,long long r,long long i,long long j,long long k)
+{
+	if (i<=l&&r<=j)
+	{
+		Node[root].val+=k*(r-l+1);
+		Node[root].tag+=k;
 		return;
 	}
-	long long mid = (l + r) / 2;
-	build(ls(f), a, l, mid);
-	build(rs(f), a, mid + 1, r);
-	PushUpSum(f);
+	PushSum(root,l,r);
+	long long mid=(l+r)/2;
+	if (i<=mid)
+		updata(L(root),l,mid,i,j,k);
+	if (j>mid)
+		updata(R(root),mid+1,r,i,j,k);
+	PushUp(root);
 }
 
-long long ST::query(long long f, long long i, long long j, long long l, long long r)
+long long St::query(long long root,long long l,long long r,long long i,long long j)
 {
-	if (i >= l && j <= r)
-		return tree[f].val;
-	PushDown(f, i, j);
-	long long mid = (i + j) / 2;
-	long long res = 0;
-	if (l <= mid)
-		res += query(ls(f), i, mid, l, r);
-	if (r > mid)
-		res += query(rs(f), mid + 1, j, l, r);
+	if (i<=l&&r<=j)
+		return Node[root].val;
+	PushSum(root,l,r);
+	long long mid=(l+r)/2;
+	long long res=0;
+	if (i<=mid)
+		res+=query(L(root),l,mid,i,j);
+	if (j>mid)
+		res+=query(R(root),mid+1,r,i,j);
 	return res;
 }
+//----------------------------------------
 
-void ST::updata(long long f, long long i, long long j, long long l, long long r, long long val)
-{
-	if (i >= l && j <= r)
-	{
-		tree[f].val += val * (j - i + 1);
-		tree[f].mark += val;
-		return;
-	}
-	PushDown(f, i, j);
-	long long mid = (i + j) / 2;
-	if (l <= mid)
-		updata(ls(f), i, mid, l, r, val);
-	if (r > mid)
-		updata(rs(f), mid + 1, j, l, r, val);
-	PushUpSum(f);
-}
-
-ST st;
-long long a[Max], n, m;
+St st;
 
 int main()
 {
-	st.init();
-	cin >> n >> m;
-	for (long long i = 1; i <= n; i++)
-		cin >> a[i];
-	st.build(1, a, 1, n);
-	long long k, x, y, g;
-	for (long long i = 1; i <= m; i++)
-	{
-		cin >> g;
-		switch (g)
-		{
-		case 1:
-			cin >> x >> y >> k;
-			st.updata(1, 1, n, x, y, k);
-			break;
-		case 2:
-			cin >> x >> y;
-			cout << st.query(1, 1, n, x, y) << endl;
-			break;
-		}
-	}
 	return 0;
 }
