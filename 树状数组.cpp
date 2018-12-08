208636b27ce7499095954c6a6cfa793c1f3b84f4
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2018-12-08 20:22:49 +0800

    Update 树状数组.cpp

--- 树状数组.cpp
+++ 树状数组.cpp
@@ -15,45 +15,45 @@ using namespace std;
 //--------------------------------
 template <typename T> class BIT
 {
-	T *tree;
-	unsigned long long n;
-	BIT();
-	inline unsigned long long lowbit(unsigned long long x);
-	public:
-		BIT(unsigned long long x);
-		void Clear();
-		void Add(unsigned long long k,T x);
-		void Query(unsigned long long k);
+    T *tree;
+    unsigned long long n;
+    BIT();
+    inline unsigned long long lowbit(unsigned long long x);
+    public:
+        BIT(unsigned long long x);
+        void Clear();
+        void Add(unsigned long long k,T x);
+        T Query(unsigned long long k);
 };
-
+  
 template <typename T> inline unsigned long long BIT<T>::lowbit(unsigned long long x)
 {
-	return x&(-x);
+    return x&(-x);
 }
-
+  
 template <typename T> BIT<T>::BIT(unsigned long long x):n(x)
 {
-	tree=(int*) malloc(sizeof(T) * x);
-	memset(tree,0,sizeof tree);
+    tree=(int*) malloc(sizeof(T) * x);
+    memset(tree,0,sizeof(T) * n);
 }
-
+  
 template <typename T> void BIT<T>::Clear()
 {
-	memset(tree,0,sizeof tree);
+    memset(tree,0,sizeof(T) * n);
 }
-
+  
 template <typename T> void BIT<T>::Add(unsigned long long k,T x)
 {
-	for (T i=k;i<=n;i+=lowbit(i))
-		tree[i]+=x;
+    for (unsigned long long i=k;i<=n;i+=lowbit(i))
+        tree[i]+=x;
 }
-
-template <typename T> void BIT<T>::Query(unsigned long long k)
+  
+template <typename T> T BIT<T>::Query(unsigned long long k)
 {
-	T ans=0;
-	for (int i=k;i>0;i-=lowbit[i])
-		ans+=tree[i];
-	return ans;
+    T ans=0;
+    for (unsigned long long i=k;i>0;i-=lowbit(i))
+        ans+=tree[i];
+    return ans;
 }
 //----------------------------------------
 
