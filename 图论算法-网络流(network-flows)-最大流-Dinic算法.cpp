dd0d712dc0c04262715d41c6b98425a31640161f
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2019-02-23 16:55:42 +0800

    Update 图论算法-网络流(network-flows)-最大流-Dinic算法.cpp

--- 图论算法-网络流(network-flows)-最大流-Dinic算法.cpp
+++ 图论算法-网络流(network-flows)-最大流-Dinic算法.cpp
@@ -1,109 +1,76 @@
-#include<iostream>
-#include<cstring>
-#include<cstdio>
-#include<string>
-#include<cstdlib>
-#include<algorithm>
-#include<set>
-#include<queue>
-#include<vector>
-#include<map>
-#define Max 1100000
+#include<bits/stdc++.h>
 using namespace std;
-
-
-class NetworkFlows
+class NetworkFlow
 {
-	struct Node
-	{
-		int y,v,next;
-	}node[Max];	
-	int dist[Max];
-	int len,last[Max],s,t,n,m,mf;
-	int find(int x,int lf);
-	void build(int x,int y,int v);
-	bool fdinic();
-	public:
-		void init();
-		void dinic();
-		void print();
+    struct Edge
+    {
+        int y,next,f;
+    }e[10000000];
+    int len,last[10000000],dis[10000000];
+    
+    bool spfa(int s,int t)
+    {
+        memset(dis,0x3f,sizeof dis);
+        dis[s]=0;
+        queue<int> que;
+        que.push(s);
+        while (!que.empty())
+        {
+            int x=que.front();
+            que.pop();
+            for (int i=last[x];i;i=e[i].next)
+            {
+                if (e[i].f&&dis[e[i].y]>dis[x]+1)
+                    dis[e[i].y]=dis[x]+1,que.push(e[i].y);
+            }
+        }
+        return dis[t]!=0x3f3f3f3f;
+    }
+    int dfs(int x,int f,int t)
+    {
+        if (x==t)
+            return f;
+        int k=0,used=0;
+        for (int i=last[x];i;i=e[i].next)
+        {
+            if (e[i].f&&dis[e[i].y]==dis[x]+1)
+            {
+                if (k=dfs(e[i].y,min(f-used,e[i].f),t))
+                    used+=k,e[i].f-=k,e[i^1].f+=k;
+                if (used==f)
+                    break;
+            }
+        }
+        return used;
+    }
+    public:
+        NetworkFlow():len(1){};
+        void add(int x,int y,int f)
+        {
+            e[++len]={y,last[x],f};
+            last[x]=len;
+            e[++len]={x,last[y],0};
+            last[y]=len;
+        }
+        int flow(int s,int t)
+        {
+            int flow=0,k=0;
+            while (spfa(s,t))
+                while (k=dfs(s,0x3f3f3f3f,t))
+                    flow+=k;
+            return flow;
+        }
 };
-
-void NetworkFlows::build(int x,int y,int v)
-{
-	len++;
-	node[len].y=y,node[len].v=v,node[len].next=last[x],last[x]=len;
-}
-
-void NetworkFlows::init()
-{
-	memset(node,0,sizeof node);
-	int x,y,v;
-	len=1;
-	cin>>n>>m>>s>>t;
-	for (int i=1;i<=m;i++)
-		cin>>x>>y>>v,build(x,y,v),build(y,x,0);
-}
-
-bool NetworkFlows::fdinic()
-{
-	memset(dist,0x7f,sizeof dist);
-	queue<int> que;
-	dist[s]=0;
-	que.push(s);
-	while (que.size())
-	{
-		int x=que.front();
-		que.pop();
-		for (int i=last[x];i;i=node[i].next)
-		{
-			int y=node[i].y;
-			if (node[i].v&&dist[y]>dist[x]+1)
-				dist[y]=dist[x]+1,que.push(y);
-		}
-	}
-	return dist[t]!=0x7f7f7f7f;
-}
-
-int NetworkFlows::find(int x,int lf)
-{
-	if (x==t)
-		return lf;
-	int rlf=0;
-	for (int i=last[x];i;i=node[i].next)
-	{
-		int y=node[i].y;
-		if (node[i].v&&dist[y]==dist[x]+1)
-			if (rlf=find(y,min(lf,node[i].v)))
-			{
-				node[i].v-=rlf,node[i^1].v+=rlf;
-				return rlf;
-			}	
-	}
-	return 0;
-}
-
-void NetworkFlows::print()
-{
-	cout<<mf<<endl;
-}
-
-void NetworkFlows::dinic()
-{
-	mf=0;
-	int lowf=0;
-	while (fdinic())
-		while (lowf=find(s,1<<26))
-			mf+=lowf;
-}
-
-
-NetworkFlows f;
-
+NetworkFlow T;
 int main()
 {
-	f.init();
-	f.dinic();
-	f.print();
-	return 0;
+    int n,m,s,t;
+    cin>>n>>m>>s>>t;
+    for (int i=1,x,y,f;i<=m;i++)
+    {
+        cin>>x>>y>>f;
+        T.add(x,y,f);
+    }
+    cout<<T.flow(s,t);
+    return 0;
 }
