2b0631cc74a3835aeac9a6a2097f5b31acb9302f
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2019-07-23 10:13:15 +0800

    Update AC自动机.cpp

--- AC自动机.cpp
+++ AC自动机.cpp
@@ -1,125 +1,87 @@
-#include<iostream>
-#include<cstring>
-#include<cstdio>
-#include<string>
-#include<cstdlib>
-#include<algorithm>
-#include<set>
-#include<queue>
-#include<vector>
-#include<map>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-//------------------------------------------------
-class AC
+template <int N, int M, int K>
+class ACA
 {
-	const int ROOT;
 	struct Node
-	{   
-		int alpha[26];
-		inline int &operator[] (int k);
+	{
+		int alpha[N];
+		int &operator[](const int &x)
+		{
+			return alpha[x];
+		}
 	};
-	Node node[1000000];
-	int cnt[1000000],fail[1000000],len;
-	
-	public:
-		AC();
-		void Init();
-		void Insert(string s);
-		void MakeFail();
-		int Query(string s);
+	Node node[M];
+	int cnt[M], fail[M], tot;
+	const int ROOT;
+	queue<int> que;
+
+public:
+	ACA() : ROOT(0), tot(0){};
+	void insert(const string &s)
+	{
+		int p = ROOT;
+		int size = s.size();
+		for (int i = 0; i != size; i++)
+		{
+			if (node[p][s[i] - K] == 0)
+				node[p][s[i] - K] = ++tot;
+			p = node[p][s[i] - K];
+		}
+		cnt[p]++;
+	}
+	void makeFail()
+	{
+		while (!que.empty())
+			que.pop();
+		memset(fail, 0, sizeof(fail));
+		for (int i = 0; i != N; i++)
+			if (node[ROOT][i] != 0)
+				que.push(node[ROOT][i]);
+		while (!que.empty())
+		{
+			int p = que.front();
+			que.pop();
+			for (int i = 0; i != N; i++)
+			{
+				if (node[p][i] != 0)
+					fail[node[p][i]] = node[fail[p]][i], que.push(node[p][i]);
+				else
+					node[p][i] = node[fail[p]][i];
+			}
+		}
+	}
+	int query(const string &s)
+	{
+		int ans = 0;
+		int p = ROOT;
+		int size = s.size();
+		for (int i = 0; i != size; i++)
+		{
+			p = node[p][s[i] - K];
+			for (int k = p; k != 0 && cnt[k] != -1; k = fail[k])
+				ans += cnt[k], cnt[k] = -1;
+		}
+		return ans;
+	}
 };
 
-AC::AC():ROOT(0)
-{
-	Init();
-}
-
-int& AC::Node::operator[] (int k)
-{
-	return alpha[k];
-}
-
-void AC::Init()
-{
-	memset(node,0,sizeof node);
-	memset(cnt,0,sizeof cnt);
-	memset(fail,0,sizeof fail);
-	len=0;
-}
-
-void AC::Insert(string s)
-{
-	int p=ROOT;
-	for (int i=0;i!=s.size();i++)
-	{
-		if (node[p][s[i]-'a']==0)
-			node[p][s[i]-'a']=++len;
-		p=node[p][s[i]-'a'];
-	}
-	cnt[p]++;
-}
-
-void AC::MakeFail()
-{
-	queue<int> que;
-	for (int i=0;i!=26;i++)
-		if (node[ROOT][i]!=0)
-			que.push(node[ROOT][i]);
-	while (!que.empty())
-	{
-		int p=que.front();que.pop();
-		for (int i=0;i!=26;i++)
-		{
-			if (node[p][i]!=0)
-			{
-				fail[node[p][i]]=node[fail[p]][i];
-				que.push(node[p][i]);
-			}
-			else
-			{
-				node[p][i]=node[fail[p]][i];
-			}
-		}
-	}
-}
-
-int AC::Query(string s)
-{
-	int ans=0,p=ROOT;
-	for (int i=0;i!=s.size();i++)
-	{
-		p=node[p][s[i]-'a'];
-		for (int k=p;k!=0&&cnt[k]!=0;k=fail[k])
-		{
-			ans+=cnt[k];
-			cnt[k]=0;
-		}
-	}
-	return ans;
-}
-//------------------------------------------------
-
-
-AC ac;
+ACA<26, 1000000, 'a'> t;
 
 int main()
 {
-	/*
-	//Example in Luogu P3808
 	int n;
-	cin>>n;
-	for (int i=1;i<=n;i++)
+	cin >> n;
+	for (int i = 1; i <= n; i++)
 	{
 		string s;
-		cin>>s;
-		ac.Insert(s);
+		cin >> s;
+		t.insert(s);
 	}
-	ac.MakeFail();
+	t.makeFail();
 	string s;
-	cin>>s;
-	cout<<ac.Query(s);
-	*/
+	cin >> s;
+	cout << t.query(s);
 	return 0;
-} 
+}
