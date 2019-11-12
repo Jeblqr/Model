0d92b695fc7fea0bad916fcfcf5ddde68440eeea
Jeblqr <33983963+Jeblqr@users.noreply.github.com>
2019-11-12 17:35:07 +0800

    Update 可持久化线段树.cpp

--- 可持久化线段树.cpp
+++ 可持久化线段树.cpp
@@ -1,72 +1,64 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+const int Maxn = 210001;
 using namespace std;
 
+class HJTTree {
+    struct Node {
+        int l, r, sum;
+    } node[Maxn << 5];
+    int cnt;
 
-//------------------------------------------------
-class PersistentSegmentTree
-{
-	#define MaxN 100000
-	#define MaxM 10000
-	struct SegmentTree
-	{
-		int l,r;
-		int dat;
-	};
-	SegmentTree tree[4*MaxN];
-	int tot,root[MaxM];
-	PersistentSegmentTree(){};
-	void pushUp(int now,int l,int r);// written by yourself
-	int build(int l,int r,int *a);
-	int insert(int now,int l,int r,int x,int val);
-};
+   public:
+    int root[Maxn];
+    void build(int& p, int l, int r) {
+        p = ++cnt;
+        if (l == r)
+            return;
+        int mid = (l + r) / 2;
+        build(node[p].l, l, mid);
+        build(node[p].r, mid + 1, r);
+    }
+    void change(int& p, int q, int l, int r, int x) {
+        p = ++cnt;
+        node[p] = node[q];
+        node[p].sum += 1;
+        if (l == r)
+            return;
+        int mid = (l + r) / 2;
+        if (x <= mid)
+            change(node[p].l, node[q].l, l, mid, x);
+        else
+            change(node[p].r, node[q].r, mid + 1, r, x);
+    }
+    int query(int p, int q, int l, int r, int k) {
+        if (l == r)
+            return l;
+        int mid = (l + r) / 2;
+        int tmp = node[node[q].l].sum - node[node[p].l].sum;
+        if (tmp >= k)
+            return query(node[p].l, node[q].l, l, mid, k);
+        else
+            return query(node[p].r, node[q].r, mid + 1, r, k - tmp);
+    }
+} T;
 
-int PersistentSegmentTree::build(int l,int r,int *a)
-{
-	int p=++tot;
-	if (l==r)
-	{
-		tree[p].dat=a[l];
-		return p;
-	}
-	int mid=(l+r)/2;
-	tree[p].l=build(l,mid,a);
-	tree[p].r=build(mid+1,r,a);
-	pushUp(p,tree[p].l,tree[p].r);
-	return p;
-	// in main()
-	// T.root[0]=build(1,n,a);
-}
+int a[Maxn], b[Maxn];
 
-int PersistentSegmentTree::insert(int now,int l,int r,int x,int val)
-{
-	int p=++tot;
-	tree[p]=tree[now];
-	if (l==r)
-	{
-		tree[p].dat=val;
-		return p;
-	}
-	int mid=(l+r)/2;
-	if (x<=mid)
-		tree[p].l=insert(tree[now].l,l,mid,x,val);
-	else
-		tree[p].r=insert(tree[now].r,mid+1,r,x,val);
-	pushUp(p,tree[p].l,tree[p].r);
-	return p;
-	// in main()
-	// root[i]=insert(root[i-1],1,n,x,val)
-}
-//------------------------------------------------
-
-class T: private PersistentSegmentTree
-{
-	void pushUp(int now,int l,int r)
-	{
-		tree[now].dat=max(tree[l].dat,tree[r].dat);
-	}
-};
-
-int main()
-{
-	return 0;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i], b[i] = a[i];
+    sort(b + 1, b + n + 1);
+    int size = unique(b + 1, b + n + 1) - b - 1;
+    T.build(T.root[0], 1, size);
+    for (int i = 1; i <= n; i++)
+        T.change(T.root[i], T.root[i - 1], 1, size,
+                 (lower_bound(b + 1, b + size + 1, a[i]) - b));
+    while (m --> 0) {
+        int x, y, k;
+        cin >> x >> y >> k;
+        cout << b[T.query(T.root[x - 1], T.root[y], 1, size, k)] << endl;
+    }
+    return 0;
 }
